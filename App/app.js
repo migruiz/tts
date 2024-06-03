9d2ace14bcee74101c16150f9983285f7a8d40f6
Miguel Ruiz <mig.ruiz@gmail.com>
2024-06-04 00:53:03 +0100

    reading serial

--- App/app.js
+++ App/app.js
@@ -2,6 +2,30 @@ const { Observable, from, of } = require('rxjs');
 
 const {  map,shareReplay,startWith, filter,switchMap, distinctUntilChanged} = require('rxjs/operators');
 
+
+const { SerialPort } = require('serialport');
+const {ReadlineParser } = require('@serialport/parser-readline');
+
+
+var port  = new SerialPort({
+    path: '/dev/ttyS0',
+    baudRate:9600,
+    dataBits:8,
+    parity: 'none',
+    flowControl: false
+});
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+
+
+// Start listening to UART data
+parser.on('open', function(data) {
+    console.log('Serial port is open');
+    parser.on('data', (data) => {
+      console.log('Received data from UART:', data);
+    });
+  });
+
+
 console.log("Started")
 
 
