226f6c8dac62a6cd239419a84ab0034e501cbda3
Miguel Ruiz <mig.ruiz@gmail.com>
2024-06-05 15:22:16 +0100

    logging outpuy

--- App/app.js
+++ App/app.js
@@ -1,31 +1,37 @@
 const { Observable, from, of } = require('rxjs');
 
 
-const {  map,shareReplay,startWith, filter,switchMap, distinctUntilChanged} = require('rxjs/operators');
+const { map, shareReplay, startWith, filter, switchMap, distinctUntilChanged } = require('rxjs/operators');
 
 
 const { SerialPort } = require('serialport');
-const {ReadlineParser } = require('@serialport/parser-readline');
+const { ReadlineParser } = require('@serialport/parser-readline');
 var spawn = require('child_process').spawn;
 const piper = spawn('/piper/process/piper --model /piper/model.onnx --config /piper/config.onnx.json --output-raw |   aplay -r 22050 -f S16_LE -t raw -', [], { shell: true });
 piper.stdin.setEncoding('utf-8');
 
-var port  = new SerialPort({
-    path: '/dev/ttyS0',
-    baudRate:9600,
-    dataBits:8,
-    parity: 'none',
-    flowControl: false
+var port = new SerialPort({
+  path: '/dev/ttyS0',
+  baudRate: 9600,
+  dataBits: 8,
+  parity: 'none',
+  flowControl: false
 });
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
 
 
 parser.on('data', (data) => {
-    
-    piper.stdin.write(data  + "\r\n")
-    console.log('Received data from UART:', data);
-  });
 
+  piper.stdin.write(data + "\r\n")
+  console.log('Received data from UART:', data);
+});
+piper.stdout.on('data', (data) => {
+  console.log(data);
+});
+piper.stderr.on('data', (data) => {
+  console.log('ERROR!!\r\n');
+  console.log(data);
+});
 
 console.log("Started")
 
