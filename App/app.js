35210979a0a0fa4e32b7788174687635edff175f
Miguel Ruiz <mig.ruiz@gmail.com>
2024-06-06 07:52:24 +0100

    recreating process

--- App/app.js
+++ App/app.js
@@ -7,10 +7,7 @@ const { map, shareReplay, startWith, filter, switchMap, distinctUntilChanged } =
 const { SerialPort } = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline');
 var spawn = require('child_process').spawn;
-const piper = spawn('/piper/process/piper --model /piper/model.onnx --config /piper/config.onnx.json --output-raw |   aplay -r 22050 -f S16_LE -t raw -', [], { shell: true });
-piper.stdin.setEncoding('utf8');
-piper.stderr.setEncoding('utf8');
-piper.stdout.setEncoding('utf8');
+
 
 var port = new SerialPort({
   path: '/dev/ttyS0',
@@ -19,27 +16,47 @@ var port = new SerialPort({
   parity: 'none',
   flowControl: false
 });
-const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+const serialDataStream = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+
+const runPiper = (command) => {
+  const piper = spawn(command, [], { shell: true });
+  piper.stdin.setEncoding('utf8');
+  piper.stderr.setEncoding('utf8');
+  piper.stdout.setEncoding('utf8');
 
 
-parser.on('data', (data) => {
+  const onSerialData = (data) => {
+    console.log('UART:', data);
+    if (data.length < 100) {
+      piper.stdin.write(data + "\r\n")
+    }
+  };
 
-  piper.stdin.write(data + "\r\n")
-  console.log('Received data from UART:', data);
-});
-piper.stdout.on('data', (data) => {
-  console.log(data);
-});
-piper.stderr.on('data', (data) => {
-  console.log(data);
-});
-piper.on('close', (code) => {
-  console.log(`child process close all stdio with code ${code}`);
-});
+  serialDataStream.on('data', onSerialData);
 
-piper.on('exit', (code) => {
-  console.log(`child process exited with code ${code}`);
-}); 
+  piper.stdout.on('data', (data) => {
+    console.log(`O-${piper.pid}: ${data}`);
+  });
+  piper.stderr.on('data', (data) => {
+    console.log(`E-${piper.pid}: ${data}`);
+  });
+  piper.on('close', (code) => {
+    console.log(`process ${piper.pid} closed with code ${code}`);
+    serialDataStream.removeListener('data', onSerialData)
+    setTimeout(() => {      
+      piper.removeAllListeners()
+    }, 1)
+    runPiper(command)    
+  });
+
+  piper.on('exit', (code) => {
+    console.log(`process ${piper.pid} exited with code ${code}`);
+  });
+
+}
+
+runPiper('/piper/process/piper --model /piper/model.onnx --config /piper/config.onnx.json --output-raw |   aplay -r 22050 -f S16_LE -t raw -');
+
 
 console.log("Started")
 
