d2b2f4177bb456395ce8e3072ceb373a54fff8ac
Miguel Ruiz <mig.ruiz@gmail.com>
2024-06-08 05:31:18 +0100

    serialreader rstart onerror

---
+++ App/SerialReader.js
@@ -0,0 +1,55 @@
+
+
+const { SerialPort } = require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline');
+
+const SerialReader = (onRobotData) => {
+
+    const initNewPort = () => {
+        const port = new SerialPort({
+            path: '/dev/ttyS0',
+            baudRate: 9600,
+            dataBits: 8,
+            parity: 'none',
+            flowControl: false
+        });
+        const serialDataStream = port.pipe(new ReadlineParser({ delimiter: '\r\n', encoding: "latin1" }));
+
+        port.on('error', function (err) {
+            console.log(`err + ${err}`);
+            setTimeout(() => {
+                //port.removeAllListeners()
+                //serialDataStream.removeAllListeners()
+                initNewPort()
+            }, 1)
+        })
+
+        port.on('close', function (err) {
+            console.log(`close ${err}`);
+        });
+
+        const onSerialData = (data) => {
+            console.log('UART: ', data);
+            try {
+                const robotData = JSON.parse(data)
+                onRobotData(robotData)
+            } catch {
+            }
+        };
+
+        serialDataStream.on('data', onSerialData);
+
+
+    }
+
+    const startReading = () => {
+        initNewPort()
+    }
+
+
+    return {
+        startReading
+    }
+}
+
+module.exports = SerialReader
--- App/app.js
+++ App/app.js
@@ -1,52 +1,33 @@
 
-
-const { SerialPort } = require('serialport');
-const { ReadlineParser } = require('@serialport/parser-readline');
 const TTS = require('./TTS')
 const SoundSelector = require('./SoundSelector')
+const SerialReader = require('./SerialReader')
 
 const tts = TTS()
 tts.startTTSProcesses();
 
 const soundSelector = SoundSelector();
 
-var port = new SerialPort({
-  path: '/dev/ttyS0',
-  baudRate: 9600,
-  dataBits: 8,
-  parity: 'none',
-  flowControl: false
-});
-const serialDataStream = port.pipe(new ReadlineParser({ delimiter: '\r\n', encoding: "latin1" }));
 
 
-const onSerialData = (data) => {
-  console.log('UART: ', data);
-  try {
-    const robotData = JSON.parse(data)
-    if (robotData.type === 'TTS') {
-      tts.handleTTSMessage(robotData)
-    }
-    if (robotData.type === 'PLAY_SOUND' || robotData.type === 'STOP_ALL_SOUNDS') {
-      soundSelector.handleSoundMessage(robotData)
-    }
-    if (robotData.type==='Init'){
-      soundSelector.stopAllSounds();
-    }
-  } catch {
+const serialReader = SerialReader(onRobotSerialData)
+
+
+
+
+const onRobotSerialData = (robotData) => {
+  if (robotData.type === 'TTS') {
+    tts.handleTTSMessage(robotData)
   }
-};
+  if (robotData.type === 'PLAY_SOUND' || robotData.type === 'STOP_ALL_SOUNDS') {
+    soundSelector.handleSoundMessage(robotData)
+  }
+  if (robotData.type === 'Init') {
+    soundSelector.stopAllSounds();
+  }
+}
 
-serialDataStream.on('data', onSerialData);
-
-port.on('error', function(err) {
-  console.log(err);
-})
-
-port.on('close', function(err) {
-  console.log(err);
-});
-
+serialReader.startReading();
 
 console.log("Started")
 
