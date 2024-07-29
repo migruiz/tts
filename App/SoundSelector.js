399ee889f054e65cacd9283ea18b9ec7a21bd223
Miguel Ruiz <mig.ruiz@gmail.com>
2024-07-29 19:34:20 +0100

    icecream sounds

--- App/SoundSelector.js
+++ App/SoundSelector.js
@@ -3,6 +3,8 @@ const Sound = require('./Sound')
 const SoundSelector = () => {
     const policeSound = Sound('/sounds/police.wav')
     const discoSound = Sound('/sounds/disco.wav')
+    const icecream1 = Sound('/sounds/icecream1.wav')
+    const icecream2 = Sound('/sounds/icecream2.wav')
 
     const handleSoundMessage = (data) => {
         if (data.type === 'PLAY_SOUND') {
@@ -12,6 +14,12 @@ const SoundSelector = () => {
             }
             else if (sound === 1) {
                 discoSound.startProcessToPlaySound()
+            }
+            else if (sound === 2) {
+                icecream1.startProcessToPlaySound()
+            }
+            else if (sound === 3) {
+                icecream2.startProcessToPlaySound()
             }
         }
         else if (data.type === 'STOP_ALL_SOUNDS') {
